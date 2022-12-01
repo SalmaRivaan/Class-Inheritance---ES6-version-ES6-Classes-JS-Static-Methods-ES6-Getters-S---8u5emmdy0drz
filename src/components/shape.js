f1397179f0ed414e5a6a830dcdb763bcf3a3f591
Salma Shaik <111558265+SalmaRivaan@users.noreply.github.com>
2022-12-02 03:10:15 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
-// declare class
+class Shape {
+    constructor() {
+        this.color = "#FF40FF";
+        this.state = () => { return 5 }
+    }
+    drawShape() {
+        return this.color
+    }
+    calculateArea() {
+        return 1;
+    }
+}
 
-// export class using module.exports
+module.exports = Shape
